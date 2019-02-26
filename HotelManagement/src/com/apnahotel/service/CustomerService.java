b50a93fd1e4bed7b06d6f74a0c05fbbe978d3e29
AparnaMishra29 <38147444+AparnaMishra29@users.noreply.github.com>
2019-02-26 10:03:40 -0800

    Update CustomerService.java

--- HotelManagement/src/com/apnahotel/service/CustomerService.java
+++ HotelManagement/src/com/apnahotel/service/CustomerService.java
@@ -3,6 +3,6 @@ package HotelManagement.src.com.service;
 import java.sql.SQLException;
 
 public interface CustomerService {
-    public boolean validate(String CustomerId) throws ClassNotFoundException, SQLException;
+    public boolean validate(String CustomerId,String password) throws ClassNotFoundException, SQLException;
 	
 }
